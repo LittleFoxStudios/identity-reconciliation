6842b1c4f087474956a6ebedde2aa06dbf6a1872
LittleFoxStudios <developers.littlefoxstudios@gmail.com>
2024-06-01 16:22:33 +0530

    Initial Commit - Added base dependencies using Spring Initializr

---
+++ src/test/java/com/littlefoxstudios/identityreconciliation/IdentityreconciliationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.littlefoxstudios.identityreconciliation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityreconciliationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
